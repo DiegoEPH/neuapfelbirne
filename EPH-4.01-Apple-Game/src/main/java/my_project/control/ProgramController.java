19870b889e6c2623700f5b31abd19a6d3bd48e5f
giandieg <giandieg@kkg-do.eu>
2023-03-01 14:58:50 +0100

    Initial commit

---
+++ EPH-4.01-Apple-Game/src/main/java/my_project/control/ProgramController.java
@@ -0,0 +1,75 @@
+package my_project.control;
+
+import KAGO_framework.control.ViewController;
+import my_project.Config;
+import my_project.model.Apple;
+import my_project.model.Pear;
+import my_project.model.Player;
+
+/**
+ * Ein Objekt der Klasse ProgramController dient dazu das Programm zu steuern. Die updateProgram - Methode wird
+ * mit jeder Frame im laufenden Programm aufgerufen.
+ */
+public class ProgramController {
+
+    //Attribute
+
+
+    // Referenzen
+    private ViewController viewController;  // diese Referenz soll auf ein Objekt der Klasse viewController zeigen. Über dieses Objekt wird das Fenster gesteuert.
+
+    private Apple apple01;
+    private Pear pear01;
+    private Player player01;
+
+    /**
+     * Konstruktor
+     * Dieser legt das Objekt der Klasse ProgramController an, das den Programmfluss steuert.
+     * Damit der ProgramController auf das Fenster zugreifen kann, benötigt er eine Referenz auf das Objekt
+     * der Klasse viewController. Diese wird als Parameter übergeben.
+     * @param viewController das viewController-Objekt des Programms
+     */
+    public ProgramController(ViewController viewController){
+        this.viewController = viewController;
+    }
+
+    /**
+     * Diese Methode wird genau ein mal nach Programmstart aufgerufen.
+     * Sie erstellt die leeren Datenstrukturen, zu Beginn nur eine Queue
+     */
+    public void startProgram() {
+        double xPos = Math.random()*(Config.WINDOW_WIDTH-50) + 50;
+        double yPos = Math.random()*(Config.WINDOW_HEIGHT-50) + 50;
+        apple01 = new Apple(xPos, yPos);
+        viewController.draw(apple01);
+
+        xPos = Math.random()*(Config.WINDOW_WIDTH-50) + 50;
+        yPos = Math.random()*(Config.WINDOW_HEIGHT-50) + 50;
+        pear01 = new Pear(xPos, yPos);
+        viewController.draw(pear01);
+
+        player01 = new Player(50, Config.WINDOW_HEIGHT-100);
+        viewController.draw(player01);
+        viewController.register(player01);
+    }
+
+    /**
+     * Aufruf mit jeder Frame
+     * @param dt Zeit seit letzter Frame
+     */
+    public void updateProgram(double dt){
+        //TODO 08 Nachdem Sie die TODOs 01-07 erledigt haben: Setzen Sie um, dass im Falle einer Kollision (siehe TODO 06 bzw. 07) zwischen dem Spieler und dem Apfel bzw. dem Spieler und der Birne, die jumpBack()-Methode von dem Apfel bzw. der Birne aufgerufen wird.
+        //Weitere TODOs folgen und werden im Unterricht formuliert. Spätestens nach TODO 08 sollte der Aufbau des Projekts durchdacht werden.
+    }
+public boolean checkAndHandleCollision(Apple a){
+        if(player01.collidesWith(a));
+        return true;
+
+}
+    //TODO 06 Fügen Sie eine Methode checkAndHandleCollision(Apple a) hinzu. Diese gibt true zurück, falls das Apple-Objekt mit dem Player-Objekt kollidiert. Nutzen Sie hierzu die collidesWith-Methode der Klasse GraphicalObject.
+    public boolean checkAndHandleCollision(Pear p){
+        if(player01.collidesWith(p));
+        return true;
+    //TODO 07 Fügen Sie eine Methode checkAndHandleCollision(Pear p) hinzu. Diese gibt true zurück, falls das Pear-Objekt mit dem Player-Objekt kollidiert. Nutzen Sie hierzu die collidesWith-Methode der Klasse GraphicalObject.
+}
+}
---
+++ EPH-4.01-Apple-Game/src/main/java/my_project/model/Apple.java
@@ -0,0 +1,40 @@
+package my_project.model;
+
+import KAGO_framework.model.GraphicalObject;
+import KAGO_framework.view.DrawTool;
+
+public class Apple extends GraphicalObject {
+
+    //Attribute
+    private double speed;
+
+    public Apple(double x, double y){
+        this.x = x;
+        this.y = y;
+        speed = 150;
+        radius = 30;
+    }
+
+    @Override
+    public void draw(DrawTool drawTool) {
+        drawTool.setCurrentColor(255,0,0,255);
+        drawTool.drawFilledCircle(x,y,radius);
+        drawTool.setCurrentColor(0,0,0,255);
+        drawTool.drawCircle(x,y,radius);
+    }
+
+    @Override
+    public void update(double dt) {
+        y=y+100*dt;
+        jumpBack();
+        //TODO 01 Ein Apfel soll von oben herab fallen. Sobald er unten den Bildschirmrand berührt wird die Methode jumpBack() aufgerufen (siehe TODO 02).
+    }
+    public void jumpBack(){
+        if (y>1000){
+            y=0;
+            x=Math.random()*300;
+        }
+    }
+
+    //TODO 02 Lege eine Methode jumpBack() an, die bei Aufruf das Apple-Objekt oben am oberen Bildschirmrand an einer zufälligen x-Position positioniert.
+}
